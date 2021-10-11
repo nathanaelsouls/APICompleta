215df29f24b0fd72f055fae86a13689ab3d592d3
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2021-10-11 15:43:20 -0300

    API completa, erro 404

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // ainda a ser desenvolvido
+    }
+}
